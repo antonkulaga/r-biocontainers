34334c102457742cbe4e60aeeae89690bf637e88
antonkulaga <antonkulaga@gmail.com>
2021-08-26 02:47:17 +0300

    updated libs in arrays

--- microarrays/install.R
+++ microarrays/install.R
@@ -1,17 +1,22 @@
-url <- "http://bioconductor.org/packages/3.6/bioc"
+if("BiocInstaller" %in% rownames(installed.packages())) remove.packages("BiocInstaller")
+install.packages("BiocManager")
 
-if ("BiocInstaller" %in% rownames(installed.packages()))
-remove.packages("BiocInstaller")
-install.packages("BiocInstaller", repos=url)
-
-to_install <- c("Matrix", "KernSmooth", "mgcv", "devtools", "biomaRt", "limma",
-                "affy", "lumi", "methylumi", "minfi", "stringr", "GEOquery", "GEOmetadb",
-                "ExiMiR", "AgiMicroRna", "doParallel","foreach", "RJSONIO",
-                "IlluminaHumanMethylation27k.db", "FDb.InfiniumMethylation.hg19"
+to_install <- c(
+  "devtools", #Dev tools installer
+  "tidyverse", # The tidyverse is an opinionated collection of R packages designed for data science.
+  "shiny", #web
+  "here",
+  "rstudioapi",
+  "caret",
+  "plotly", #charts
+  "optigrab", "optparse", "docopt" #for CLI
 )
+install.packages(to_install, dependencies = TRUE)
 
-for (pack in to_install)
-    if (!suppressWarnings(require(pack, character.only=TRUE)))
-        BiocInstaller::biocLite(pack)
-suppressWarnings(BiocInstaller::biocValid(fix=TRUE, ask=FALSE))
-suppressWarnings(install.packages("optparse"))
+to_install_bioconductor <- c("Matrix", "KernSmooth", "mgcv", "devtools", "biomaRt", "limma",
+                                            "affy", "lumi", "methylumi", "minfi", "stringr", "GEOquery", "GEOmetadb",
+                                            "ExiMiR", "AgiMicroRna", "doParallel","foreach", "RJSONIO", "org.Bt.eg.db",
+                                            "IlluminaHumanMethylation27k.db", "FDb.InfiniumMethylation.hg19", "hgfocus.db", "VennDiagram"
+                            )
+
+BiocManager::install(to_install_bioconductor)
