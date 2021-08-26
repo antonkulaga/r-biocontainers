b1a4f2c0a7272d7753298f9961eedab0d58c963e
antonkulaga <antonkulaga@gmail.com>
2021-08-26 04:28:52 +0300

    fix master -> latest

--- core/install.R
+++ core/install.R
@@ -9,14 +9,3 @@ to_install <- c(
   "optigrab", "optparse", "docopt" #for CLI
 )
 install.packages(to_install, dependencies = TRUE)
-
-if("BiocInstaller" %in% rownames(installed.packages())) remove.packages("BiocInstaller")
-install.packages("BiocManager")
-
-to_install_bioconductor <- c(
-    "Matrix", "KernSmooth", "mgcv", "Biostrings", "BiocGenerics", "S4Vectors",
-    "devtools", "tximport", "tximeta", #for development, counts import
-    "biomaRt", "AnnotationDbi", "ensembldb", "AHEnsDbs", #for annotations
-    "missForest", "doParallel"
-)
-BiocManager::install(to_install_bioconductor)
--- microarrays/install.R
+++ microarrays/install.R
@@ -1,6 +1,5 @@
-to_install_bioconductor <- c("Matrix", "KernSmooth", "mgcv", "devtools", "biomaRt", "limma",
-                                            "affy", "lumi", "methylumi", "minfi", "stringr", "GEOquery", "GEOmetadb",
-                                            "ExiMiR", "AgiMicroRna", "doParallel","foreach", "RJSONIO", "org.Bt.eg.db",
-                                            "IlluminaHumanMethylation27k.db", "FDb.InfiniumMethylation.hg19", "hgfocus.db", "VennDiagram"
+to_install_bioconductor <- c("mgcv", "limma", "affy", "lumi", "methylumi", "minfi", "stringr", "GEOquery", "GEOmetadb",
+  "ExiMiR", "AgiMicroRna", "doParallel","foreach", "RJSONIO", "org.Bt.eg.db",
+  "IlluminaHumanMethylation27k.db", "FDb.InfiniumMethylation.hg19", "hgfocus.db"
                             )
 BiocManager::install(to_install_bioconductor)
